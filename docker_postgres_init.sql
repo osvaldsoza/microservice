ca08276ab76f46bf604f7141975b538cd54110e1
osvaldsoza <osvald.soza@gmail.com>
2024-02-10 12:56:56 -0300

    first commit pagamentos

---
+++ docker_postgres_init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE microservice_pagamentos
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+
+CREATE DATABASE microservice_pedidos
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
